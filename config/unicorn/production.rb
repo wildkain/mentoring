e07fb62194af895df6ce1de35ea9477e6a54eb95
Stanislav <wildkain@gmail.com>
2018-10-07 14:02:25 +0300

    pids

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,7 +1,7 @@
 deploy_to  = '/var/www/mentoring'
 rails_root = "#{deploy_to}/current"
-pid_file   = "#{deploy_to}/current/tmp/pids/unicorn.pid"
-socket_file= "#{deploy_to}/current/tmp/sockets/unicorn.sock"
+pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
+socket_file= "#{deploy_to}/shared/sockets/unicorn.sock"
 log_file   = "#{rails_root}/log/unicorn.log"
 err_log    = "#{rails_root}/log/unicorn_error.log"
 old_pid    = pid_file + '.oldbin'
